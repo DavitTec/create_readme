92a2720c9c48142ea8baed50f9a59b3287b6f150
David Mullins <me@davit.ie>
2025-03-28 02:38:00 +0100

    #TEST ex_bash_rematch.sh # version 0.0.1-8

--- src/ex_bash_rematch.sh
+++ src/ex_bash_rematch.sh
@@ -1,77 +1,81 @@
 #!/usr/bin/env bash
 # src/ex_bash_rematch.sh
-# version 0.0.1-1
+# version 0.0.1-8
+# lng=en_GB  #(British English, Left to Right)
 
-# lng=en-GB  #(Right to left)
+export LC_ALL=C.UTF-8
 
-#TESTING with S1 = TEST1 to TEST5
-
+# TESTING with S1 = TEST1 to TEST4
 TEST_1="Hello=Worl=Today"
 TEST_2="HeLlo=Worl=dToday"
 TEST_3="Hello=Wor=ld=Today"
 TEST_4="Hello=w=rl=Today"
-TEST_5="فتاحي = ليس كذلك = عظيم = اليوم"
 
-## TEST 1
-# Testing for "version" string as $1
-# set 'kv' to "=" (as a  char(code))
-# set input_string
-# - convert to $1 lower case
-# - determine $1 must include a 'kv' (max 1 in count)
-# - strip $1 any hidden characters
-# - determine $1 lenght must be less than max(25 char)
-# -
-# case check if $1 (left of 'kv') is
-#  equal to "version",  do next
-#  1) set testID equal to 1
-#  2) set testID equal to 2
-#  exit
-#  not equal then exit
 echo "___>testing for key value pairs"
 
-# get key value pairs
+# Function to split and modify key-value pairs
 keyvalue() {
-  # Assume Left to Right languages
   local line=$1
   if [[ $line =~ = ]]; then
-    key=${line%%=*}
-    value=${line#*=}
-    value=${value//=/§}
+    # Find the position of the first '='
+    first_eq=$(expr index "$line" "=")
+    # Extract key and value
+    key=${line:0:first_eq-1}
+    value=${line:first_eq}
+    # Replace '=' with '_' in value
+    value=${value//=/_}
     echo "$key=$value"
   else
     echo "$line"
   fi
 }
 
-# Check for pattern match
+# Function to check pattern match and set global variables
 checkPattern() {
   local string=$1
-  echo -n "my string1= $string"
-  pattern='(h[a-z]+)\=*(w[a-z]+)' #search pattern
+  pattern='^(h[a-z]+)=*(w[a-z]+)$'
   if [[ $string =~ $pattern ]]; then
-    echo "Match found!"
-    for i in "${!BASH_REMATCH[@]}"; do
-      echo "$i: ${BASH_REMATCH[$i]}"
-    done
+    matched="Yes"
+    group1=${BASH_REMATCH[1]}
+    group2=${BASH_REMATCH[2]}
   else
-    echo "'$1' Not found!"
+    matched="No"
+    group1=
+    group2=
   fi
 }
 
-# Perform tests on strings
-for ((i = 1; i < 6; i++)); do
+# Print table headings
+printf "%-10s | %-25s | %-15s | %-25s | %-25s | %-30s\n" \
+  "Test Case" "Input String" "Key" "Value After Replacement" "Output After Processing" "Pattern Match (Expected)"
+printf "%-10s | %-25s | %-15s | %-25s | %-25s | %-30s\n" \
+  "----------" "--------------" "-----" "------------------------" "------------------------" "---------------------------"
+
+# Loop through test cases
+for ((i = 1; i <= 4; i++)); do
   mystring="TEST_$i"
-  #   checkPattern() string
-  string="$(keyvalue ${!mystring})"
-  string=${string,,}
-  echo "check pattern before using: $string"
-  checkPattern "$string"
-  #  echo " The KEY is: ${!string}"
-  # echo " The VALUE is: ${!string}"
+  test_case=$mystring
+  input_string=${!mystring}
+  
+  # Process the string using keyvalue function
+  string=$(keyvalue "$input_string")
+  key=${string%%=*}
+  value_replaced=${string#*=}
+  processed_string=${string,,}
+  
+  # Check the pattern
+  checkPattern "$processed_string"
+  
+  # Construct pattern match column
+  if [ "$matched" == "Yes" ]; then
+    pattern_match="Match: groups '$group1', '$group2'"
+  else
+    pattern_match="Not found!"
+  fi
+  
+  # Print the row
+  printf "%-10s | %-25s | %-15s | %-25s | %-25s | %-30s\n" \
+    "$test_case" "$input_string" "$key" "$value_replaced" "$processed_string" "$pattern_match"
+  
   echo
 done
-
-#string="$(keyvalue $TEST_2)"
-#string=${string,,}
-
-#echo " The KEY is: $string"
